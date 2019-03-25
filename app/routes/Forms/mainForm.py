315ab96d9eec85696832d380cfbb2a07ce371599
Jackton Levy <jacktonbrigs@gmail.com>
2019-03-24 19:36:03 -0700

    typos

--- app/routes/Forms/mainForm.py
+++ app/routes/Forms/mainForm.py
@@ -1,18 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
-
 import pandas as pd
 import os
+
+
 def getDataOptions():
     cd = os.getcwd()
-    table = pd.read_csv(cd.replace('\\','/')+'/app/routes/misc/raw.csv')
+    table = pd.read_csv(cd.replace('\\', '/') + '/app/routes/misc/raw.csv')
     colNames = []
     for i in table:
-        colNames.append("i")
+        colNames.append(i)
     data = list(zip(colNames, colNames))
     return data
+
 
 class GiveForm(FlaskForm):
     dataOptions = SelectField("Data Options", choices=getDataOptions())
     graphOptions = SelectField("Graph Options",
-    choices=[("Bar Graph", "Bar Graph"), ("Pie Chart", "Pie Chart")])
+                               choices=[("Bar Graph", "Bar Graph"), ("Pie Chart", "Pie Chart")])
