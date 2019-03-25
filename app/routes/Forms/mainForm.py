82c2588cdce43cac531c1a2207889d228b60da07
clementef <clementef2000@gmail.com>
2019-03-24 20:17:34 -0700

    final form

--- app/routes/Forms/mainForm.py
+++ app/routes/Forms/mainForm.py
@@ -2,20 +2,17 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
 import pandas as pd
 import os
-
-
-def getDataOptions():
-    cd = os.getcwd()
-    table = pd.read_csv(cd.replace('\\', '/') + '/app/routes/misc/raw.csv')
-    colNames = []
-    for i in table:
-        colNames.append(i)
-    data = list(zip(colNames, colNames))
-    return data
+cd = os.getcwd()
+table = pd.read_csv(cd.replace('\\', '/') + '/app/routes/misc/raw.csv')
+colNames = []
+for i in table:
+    colNames.append(i)
+data = list(zip(colNames, colNames))
 
 
 class MainForm(FlaskForm):
-    dataOptions = SelectField("Data Options", choices=getDataOptions())
+    xData = SelectField("xData", choices=data)
+    yData = SelectField("yData", choices=data)
     graphOptions = SelectField("Graph Options",
                                choices=[("Bar Graph", "Bar Graph"),
                                ("Pie Chart", "Pie Chart")])
